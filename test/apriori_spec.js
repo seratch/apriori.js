ad594c65c78d6fce3cbd0d8b6d6a3345d8656575
Kazuhiro Sera <seratch@gmail.com>
2014-06-27 07:45:33 +0900

    Update tests

--- test/apriori_spec.js
+++ test/apriori_spec.js
@@ -5,15 +5,26 @@ var assert = require('assert'),
     Apriori = require('../apriori.js');
 
 describe('Apriori.Algorithm', function () {
-  describe('#analyize(transactions)', function () {
-    it('should return 5 association rules when analyzing dataset.csvs', function () {
-      fs.readFile('dataset.csv', 'utf8', function (err, csv) {
-        var transactions = Apriori.ArrayUtils.readCSVToArray(csv);
-        var apriori = new Apriori.Algorithm(0.15, 0.6);
-        var result = apriori.analyze(transactions);
-        assert.equal(5, result.associationRules.length);
-      });
+    describe('#analyize(transactions)', function () {
+
+        it('should return 5 association rules with default settings', function () {
+            fs.readFile('dataset.csv', 'utf8', function (err, csv) {
+                var transactions = Apriori.ArrayUtils.readCSVToArray(csv);
+                var apriori = new Apriori.Algorithm(0.15, 0.6);
+                var result = apriori.analyze(transactions);
+                assert.equal(5, result.associationRules.length);
+            });
+        });
+
+        it('should return 4 association rules with confidence:0.8', function () {
+            fs.readFile('dataset.csv', 'utf8', function (err, csv) {
+                var transactions = Apriori.ArrayUtils.readCSVToArray(csv);
+                var apriori = new Apriori.Algorithm(0.15, 0.8);
+                var result = apriori.analyze(transactions);
+                assert.equal(4, result.associationRules.length);
+            });
+        });
+
     });
-  });
 });
 
